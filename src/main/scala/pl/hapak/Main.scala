da2a4fd148200f4166ae6f56be4a3f96ac1f90db
kamil.hapak <kamil.hapak@gmail.com>
2019-12-18 11:30:22 +0100

    throw exception if no path

--- src/main/scala/pl/hapak/Main.scala
+++ src/main/scala/pl/hapak/Main.scala
@@ -8,6 +8,10 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
+    if(args.isEmpty) {
+      throw new RuntimeException("Path to xlsx file required as 1st param!")
+    }
+
     val reader = new XlsxReader
     val cells = reader.readCellDetails(Path.of(args.head))
     val mapper = new NodeMapper
